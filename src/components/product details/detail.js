cbbe6cb18e986995872a461abf80d4c8132868c3
RowanAbdelhafizz <143328283+RowanAbdelhafizz@users.noreply.github.com>
2023-09-04 05:18:28 +0300

    Add files via upload

---
+++ src/components/product details/detail.js
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+function ProductDetails() {
+    const {id}=useParams()
+   
+    return ( <>
+    <h1>Details Page and id is {id}</h1>
+    </> );
+}
+
+export default ProductDetails;
